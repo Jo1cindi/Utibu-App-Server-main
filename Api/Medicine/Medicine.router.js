37588efd27312f854903c8b19a6aa60c9f290c54
noelwesleey64 <noelwesley8750@gmail.com>
2024-04-01 20:32:58 +0300

    sending data

--- Api/Medicine/Medicine.router.js
+++ Api/Medicine/Medicine.router.js
@@ -2,18 +2,6 @@ const router = require("express").Router();
 const sql = require("mssql/msnodesqlv8");
 
 router.get("/get-medicine", (req, res) => {
-  // res.setHeader("Access-Control-Allow-Origin", "*");
-  // res.setHeader(
-  //   "Access-Control-Allow-Methods",
-  //   "GET, POST, OPTIONS, PUT, PATCH, DELETE"
-  // );
-  // res.setHeader(
-  //   "Access-Control-Allow-Headers",
-  //   "X-Requested-With,content-type"
-  // );
-  // res.setHeader("Access-Control-Allow-Credentials", true);
-
-
   const category = req.body.category;
 
 
@@ -25,7 +13,7 @@ router.get("/get-medicine", (req, res) => {
         console.log(result)
         // res.json(result)
         res.status(200).send({
-          data: result.recordsets[0],
+          data: result.recordset,
         })
     }
     
